2b3c761cdcbd8eadb4354e10bbc46ebb8a4716e9
swagner <swagner@swagner-Laptop.(none)>
2011-08-16 13:23:52 -0400

    Added flash_rom_image command

--- board/avid/garcia/garcia.c
+++ board/avid/garcia/garcia.c
@@ -71,3 +71,39 @@ int board_eth_init(bd_t *bis)
    */
   return(labx_eth_initialize(bis));
 }
+
+int do_flash_rom_image(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
+{
+	const char script1[] = "\
+			echo Starting Garcia Flash image load\n\
+			echo --------------------------------\n\
+			set autoload no\n\
+			set ethaddr 00:0a:35:00:44:01\n\
+			setenv ipaddr 192.168.1.1\n\
+			setenv serverip 192.168.1.100\n\
+			setenv netkargs labx_eth_llink.macaddr=0x00,0x0a,0x35,0x00,0x44,0x01\n\
+			ping 192.168.1.100\n\
+			tftp 0x88001000 ";
+	const char script2[] = "\n\
+			protect off 0x87000000 +0x1000000\n\
+			erase 0x87000000 +0x1000000\n\
+			cp.b 0x88001000 0x87000000 0x1000000\n\
+			echo Flash load complete\n\
+			echo -------------------\n";
+	const char default_rom_image[] = "garcia.rom";
+	char script[sizeof(script1)+sizeof(script2)+64];
+    strcpy(script, script1);
+	if (argc < 2) {
+		strcat(script, default_rom_image);
+	} else {
+		strcat(script, argv[1]);
+	}
+	strcat(script, script2);
+	return(run_command(script, flag));
+
+}
+
+U_BOOT_CMD(flash_rom_image, 1, 0, do_flash_rom_image,
+		"Load a Garcia Flash ROM image into Flash memory",
+		"Read a flash ROM image, by default named \"garcia.rom\", from a TFTP server at"
+		" IP address 192.168.1.100 and write it to the Flash ROM.");
