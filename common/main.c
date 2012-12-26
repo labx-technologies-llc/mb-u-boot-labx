3345575c1153af953437ab03d475bfc4b7d49367
Yuriy Dragunov <yuriy.dragunov@labxtechnologies.com>
2012-12-26 15:42:25 -0500

    Removed debug printouts from main().

--- common/main.c
+++ common/main.c
@@ -317,8 +317,6 @@ void main_loop (void)
 	char bcs_set[16];
 #endif /* CONFIG_BOOTCOUNT_LIMIT */
 
-  puts("main\n");
-
 #if defined(CONFIG_VFD) && defined(VFD_TEST_LOGO)
 	ulong bmp = 0;		/* default bitmap */
 	extern int trab_vfd (ulong bitmap);
@@ -436,12 +434,10 @@ void main_loop (void)
 #endif /* CONFIG_POST */
 
 #if defined(CONFIG_LABX_PREBOOT)
-  puts("preboot\n");
   labx_preboot_res = labx_preboot(bootdelay);
 	if(labx_preboot_res == -1) bootdelay = -1;
   else if(labx_preboot_res == 0) bootdelay = 0;
 #endif
-  puts("post preboot\n");
 
 #ifdef CONFIG_BOOTCOUNT_LIMIT
 	if (bootlimit && (bootcount > bootlimit)) {
