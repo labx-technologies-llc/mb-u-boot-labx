2a7f6d057a648489190faf5c8b02f9f9a99fdad5
Albert Hajjar <ahajjar@AL-VMUBUNTUX64>
2013-04-18 13:05:18 -0400

    Renamed script variables.  Moved location of u-boot to upper half of DDR.

--- board/labx/hats/config.mk
+++ board/labx/hats/config.mk
@@ -27,6 +27,6 @@
 
 # This places the execution location for U-Boot to be
 # a little over 32 MiB into the DDR2 device.
-TEXT_BASE = 0x89F00000
+TEXT_BASE = 0x8FC00000
 
 PLATFORM_CPPFLAGS += -mno-xl-soft-mul -mno-xl-soft-div -mxl-barrel-shift -mxl-pattern-compare
