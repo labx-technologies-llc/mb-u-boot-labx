7871eafc50a7ac8bf1e37358b62a90620d845157
Chris Wulff <chris.wulff@labxtechnologies.com>
2013-07-23 11:53:18 -0400

    Also rename metalheart config file that was missed.

--- include/configs/labx_metalheart.h
+++ include/configs/labx_metalheart.h
@@ -23,10 +23,10 @@
  * MA 02111-1307 USA
  */
 
-#ifndef _LAWO_METALHEART_H_
-#define _LAWO_METALHEART_H_
+#ifndef _LABX_METALHEART_H_
+#define _LABX_METALHEART_H_
 
-#include "../board/lawo/metalheart/xparameters.h"
+#include "../board/labx/metalheart/xparameters.h"
 
 #define	CONFIG_MICROBLAZE	1	/* MicroBlaze CPU */
 #define	MICROBLAZE_V5		1
@@ -286,7 +286,7 @@
 #define	CONFIG_SYS_LOAD_ADDR	XILINX_RAM_START /* default load address */
 
 /* Some appropriate defaults for network settings */
-#define CONFIG_HOSTNAME		lawo-metalheart
+#define CONFIG_HOSTNAME		labx-metalheart
 #define CONFIG_IPADDR           192.168.1.1
 #define CONFIG_SERVERIP         192.168.1.100
 
