ab7c6bcd18cf7f095bdcb272d1597d8784b0b9b8
Albert Hajjar <ahajjar@AL-VMUBUNTUX64>
2013-07-31 14:15:55 -0400

    Updated files with latest version of AVBX platform compatible with Release 2.0.

--- board/labx/essex/avbx.c
+++ board/labx/essex/avbx.c
@@ -32,18 +32,6 @@
 #include <net.h>
 #include <netdev.h>
 
-void do_reset (void)
-{
-#ifdef CONFIG_SYS_GPIO_0
-	*((unsigned long *)(CONFIG_SYS_GPIO_0_ADDR)) =
-	    ++(*((unsigned long *)(CONFIG_SYS_GPIO_0_ADDR)));
-#endif
-#ifdef CONFIG_SYS_RESET_ADDRESS
-	puts ("Reseting board\n");
-	asm ("bra r0");
-#endif
-}
-
 int gpio_init (void)
 {
 #ifdef CONFIG_SYS_GPIO_0
